c3b0a6e042de76b3c0f1e68b8d78b45fe2903b75
Grayson Huey <96308751+GraysonHuey@users.noreply.github.com>
2024-11-22 11:12:10 -0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @echo off
+
 :: Check if Python is installed
 python --version >nul 2>&1
 if %errorlevel% neq 0 (
@@ -9,25 +10,36 @@ if %errorlevel% neq 0 (
     echo Python has been installed. Please restart the script to continue.
     exit /b
 )
+
 cls
+
 :: Upgrade pip
 echo Upgrading pip...
 python -m pip install --upgrade pip
+
 cls
+
 :: Install Pillow
 echo Installing Pillow...
 pip install pillow
+
 cls
+
 :: Install PyAutoGUI
 echo Installing PyAutoGUI...
 pip install pyautogui
+
 cls
+
 :: Prompt user to open slideshow in presentation mode
 echo Make sure you open the slideshow to presentation mode in the next 15 seconds!
 timeout /t 15 >nul
+
 :: Run main.py
 echo Running main.py...
 python main.py
+
 cls
+
 echo Program terminated!
 pause
